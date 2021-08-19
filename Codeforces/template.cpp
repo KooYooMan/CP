ab30b5057712767d4593bd5fa1169f755ddb3a1d
Manh Cao <manhcaoduy1912@gmail.com>
2021-08-19 11:19:41 +0700

    Add: Codeforces template.cpp add fast I/O

--- Codeforces/template.cpp
+++ Codeforces/template.cpp
@@ -4,5 +4,8 @@ using namespace std;
 int main() {
 	// freopen("input.txt", "r", stdin); freopen("output.txt", "w", stdout);
 
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
 	return 0;
 }
